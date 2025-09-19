edd6a71e77f74e368fb1158451436504c8640151
anton-puisho <puishoanton@gmail.com>
2025-09-19 13:26:23 +0200

    feat: Server | Add repositories

---
+++ packages/server/Fitness.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Fitness.Application.Interfaces.Repositories;
+using Fitness.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness.Infrastructure.Repositories
+{
+    public class GenericRepository<T>(AppDbContext context) : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            T? entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ packages/server/Fitness.Infrastructure/Repositories/WorkoutTemplateRepository.cs
@@ -0,0 +1,11 @@
+﻿using Fitness.Application.Interfaces.Repositories;
+using Fitness.Domain.Entities;
+using Fitness.Infrastructure.Data;
+
+namespace Fitness.Infrastructure.Repositories
+{
+    public class WorkoutTemplateRepository(AppDbContext context) : GenericRepository<WorkoutTemplate>(context), IWorkoutTemplateRepository
+    {
+        private readonly AppDbContext _context = context;
+    }
+}

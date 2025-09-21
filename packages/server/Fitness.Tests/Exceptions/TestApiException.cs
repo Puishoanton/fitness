6f590e4fbb9646df2059d7eda30ea6fffa0a3c87
anton-puisho <puishoanton@gmail.com>
2025-09-21 13:58:15 +0200

    feat: Test | Add error handling middleware tests

---
+++ packages/server/Fitness.Tests/Exceptions/TestApiException.cs
@@ -0,0 +1,8 @@
+﻿using Fitness.Application.Exceptions;
+
+namespace Fitness.Tests.Exceptions
+{
+    public class TestApiException(int statusCode, string message) : ApiException(statusCode, message)
+    {
+    }
+}

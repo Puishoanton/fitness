d75e73b7b06f7f3a8c5f187d0b98bbd8cc205dd1
anton-puisho <puishoanton@gmail.com>
2025-10-03 15:50:15 +0200

    feat: Backend | Add dtos to workout session service

---
+++ packages/server/Fitness.Application/DTOs/WorkoutSession/WorkoutSessionResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using Fitness.Application.DTOs.ExerciseLog;
+using Fitness.Domain.Enums;
+
+namespace Fitness.Application.DTOs.WorkoutSession
+{
+    public class WorkoutSessionResponseDto
+    {
+        public Guid Id { get; set; }
+        public int Duration { get; set; } = 0;
+        public int AverageRestTime { get; set; } = 0;
+        public Status Status { get; set; }
+        public List<ExerciseLogLightDto> ExerciseLogs { get; set; } = [];
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

78b471411755f64d3a1a0e223c94ba00d3833117
anton-puisho <puishoanton@gmail.com>
2025-10-12 11:17:07 +0200

    feat: Backend | Add a exercise log service

--- packages/server/Fitness.Infrastructure/Repositories/ExerciseLogRepository.cs
+++ packages/server/Fitness.Infrastructure/Repositories/ExerciseLogRepository.cs
@@ -7,5 +7,10 @@ namespace Fitness.Infrastructure.Repositories
     public class ExerciseLogRepository(AppDbContext context) : GenericRepository<ExerciseLog>(context), IExerciseLogRepository
     {
         public readonly AppDbContext _context = context;
+
+        public async Task<int> CountByWorkoutSessionIdAsync(Guid workoutSessionId)
+        {
+            return await Task.FromResult(_context.ExerciseLogs.Count(el => el.WorkoutSessionId == workoutSessionId));
+        }
     }
 }

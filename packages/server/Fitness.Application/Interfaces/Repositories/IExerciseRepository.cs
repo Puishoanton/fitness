259ace1a97b67a079c5b22c9b014168dea1d4c83
anton-puisho <puishoanton@gmail.com>
2026-04-12 18:17:16 +0200

    feat: Add endpoint get muscle groups

--- packages/server/Fitness.Application/Interfaces/Repositories/IExerciseRepository.cs
+++ packages/server/Fitness.Application/Interfaces/Repositories/IExerciseRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Fitness.Domain.Entities;
+using Fitness.Domain.Enums;
 
 namespace Fitness.Application.Interfaces.Repositories
 {
     public interface IExerciseRepository : IRepository<Exercise>
     {
+        public Task<List<MuscleGroup>> GetUniqueMuscleGroupsAsync();
     }
 }
--- packages/server/Fitness.Infrastructure/Repositories/ExerciseRepository.cs
+++ packages/server/Fitness.Infrastructure/Repositories/ExerciseRepository.cs
@@ -1,11 +1,21 @@
 ﻿using Fitness.Application.Interfaces.Repositories;
 using Fitness.Domain.Entities;
+using Fitness.Domain.Enums;
 using Fitness.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fitness.Infrastructure.Repositories
 {
     public class ExerciseRepository(AppDbContext context) : GenericRepository<Exercise>(context), IExerciseRepository
     {
         private readonly AppDbContext _context = context;
+
+        public async Task<List<MuscleGroup>> GetUniqueMuscleGroupsAsync()
+        {
+            return await _context.Exercises
+                .Select(e => e.MuscleGroup)
+                .Distinct()
+                .ToListAsync();
+        }
     }
 }
